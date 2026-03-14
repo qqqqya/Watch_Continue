05d65734919e05b6242ac7a06a85f0abdc9ed99f
qi zhang <16539587+yanqqy@user.noreply.gitee.com>
2026-03-14 09:25:51 +0800

    driver_initial

---
+++ BSP/LED/bsp_led_driver.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ * 
+ * All Rights Reserved.
+ * 
+ * @file bsp_led_driver.h
+ * 
+ * @par dependencies 
+ * - stdio.h
+ * - stdint.h
+ * 
+#include "stdint.h"
+#include "stm32f1xx_hal.h"
+#include "FreeRTOS.h"                   // ARM.FreeRTOS::RTOS:Core
+#include "semphr.h"                     // ARM.FreeRTOS::RTOS:Core
+#include "queue.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdint.h"
+ * @author Jack | R&D Dept. | EternalChip ?????
+ *
+ * @brief Provides HAL APIs for LED control and operations.
+ * 
+ * Usage:
+ * Call functions directly.
+ * 
+ * @version V1.0 2024-10-18
+ *
+ * @note 1 tab == 4 spaces
+ * 
+ *****************************************************************************/
+#ifndef __BSP_LED_DRIVER_H__
+#define __BSP_LED_DRIVER_H__
+
+
+
+#endif
