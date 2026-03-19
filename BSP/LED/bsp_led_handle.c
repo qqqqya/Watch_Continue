6625483027c8ff010251ea3c4c4d0e44852b433c
qi zhang <16539587+yanqqy@user.noreply.gitee.com>
2026-03-19 10:10:16 +0800

    Initialize_Bridge_Parttern

---
+++ BSP/LED/bsp_led_handle.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ * 
+ * All Rights Reserved.
+ * 
+ * @file bsp_led_driver.c
+ * 
+ * @par dependencies 
+ * - bsp_led_driver.h
+ * - stdint.h
+ * 
+#include "bsp_led_driver.h"
+#include "stdint.h"
+ * @author Yan | R&D Dept. | EternalChip
+ *
+ * @brief Provides HAL APIs for LED control and operations.
+ * 
+ * Usage:
+ * Call functions directly.
+ * 
+ * @version V1.0 2026-03-14
+ *
+ * @note 1 tab == 4 spaces
+ * 
+ * 
+******************************* includes ***********************************
+******************************* fuctions ***********************************
+ *****************************************************************************/
+/******************************** includes ************************************/
+ #include "bsp_led_driver.h"
+
+
+ 
---
+++ BSP/LED/bsp_led_handle.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ * 
+ * All Rights Reserved.
+ * 
+ * 
+******************************* includes ***********************************
+******************************* fuctions ***********************************
+ * @file bsp_led_driver.h
+ *  
+ * @par dependencies 
+ * - stdio.h
+ * - stdint.h
+ * 
+#include "stdio.h"
+#include "stdint.h"
+ * @author Yan | R&D Dept. | EternalChip
+ *
+ * @brief Provides HAL APIs for LED control and operations.
+ * 
+ * Usage:
+ * Call functions directly.
+ * 
+ * @version V1.0 2026-03-19
+ *
+ * @note 1 tab == 4 spaces
+
+ *****************************************************************************/
+#ifndef __BSP_LED_HANDLE_H__
+#define __BSP_LED_HANDLE_H__
+//******************************* includes ***********************************//
+#include "stdio.h"
+#include "stdint.h"
+
+
+//******************************* Defines ***********************************//
+
+
+
